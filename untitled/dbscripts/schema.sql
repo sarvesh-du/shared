f8a331118444aa8a8a18c64ef50c1c729285582a
sarvesh <sarvesh>
2014-08-21 23:49:38 +0530

    schema created

--- untitled/dbscripts/schema.sql
+++ untitled/dbscripts/schema.sql
@@ -1,1 +1,9 @@
-show databases;
+create table user_profile(
+	id int primary key,
+	name varchar(20),
+	email varchar(30) not null unique,
+	password varchar(256) not null 
+);
+
+insert into user_profile values(1,"Saravesh Kumar","sarvesh.kumar@quovantis.com","12345");
+insert into user_profile values(1,"Jatin Sehgal","jatin.sehgal@quovantis.com","12345");
